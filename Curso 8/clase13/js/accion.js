1dafc766532053bbe5ad868e18a9f11597c1e026
GeOnE <emedarias@gmail.com>
2019-06-29 10:51:04 -0300

    Agregadas la clase 13

---
+++ Curso 8/clase13/js/accion.js
@@ -0,0 +1,8 @@
+var num=10; //Entero o Int
+var coma=1.345; //Coma flotante o Float
+var nom="Emilio"; //Cadena de texto o String
+var verdarero=true; //Booleano o Bool o Boolean.
+
+var num2="10";
+
+document.write(nom);
