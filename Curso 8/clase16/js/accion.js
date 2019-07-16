09e377b1aaabc27f10cf5347c6916851e0d3554a
GeOnE <emedarias@gmail.com>
2019-07-16 20:56:24 -0300

    Agregada la clase 16

--- Curso 8/clase16/js/accion.js
+++ Curso 8/clase16/js/accion.js
@@ -3,6 +3,7 @@ var boton= document.getElementsByTagName("input");
 /*Agregamos el escuchador de eventos al boton.*/
 boton[0].addEventListener("click",cargarDatos,false);
 /*Crear la funcion "cargarDatos" y ejecutarla.*/
+
 function cargarDatos()
 	{
 		/*------ 1° paso - pedir datos. ------*/
@@ -23,4 +24,33 @@ function cargarDatos()
 		var td2=document.createElement("td");
 		/*Vamos a guardar el edad dentro del segundo <td>*/
 		td2.innerHTML=edad;
+
+		/*Crear el tercer <td>*/
+		var td3=document.createElement("td");
+		/*Preguntar si es menor o mayor a 18.*/
+		if(edad>=18)
+			{
+				td3.innerHTML="Mayor";
+			}
+		if(edad<18)
+			{
+				td3.innerHTML="Menor";
+			}
+
+		/*Crear la etiqueta de fila <tr>*/
+		var tr=document.createElement("tr");
+		/*Agregar al primer hijo, el td1*/
+		tr.appendChild(td1);
+		/*Agregar al segundo hijo, el td2*/
+		tr.appendChild(td2);
+		/*Agregar al tercer hijo, el td3*/
+		tr.appendChild(td3);
+
+
+		/*3° paso - Llamar a la etiqueta correspondiente y colocar los elementos en el HTML.*/
+
+		/*Llamamos a la etiqueta <tbody>*/
+		var tbody=document.getElementsByTagName("tbody");
+		/*Agregar a tr como hijo.*/
+		tbody[0].appendChild(tr);
 	}
