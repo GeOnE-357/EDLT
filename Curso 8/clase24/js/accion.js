f1b75de942afb6b49f56b76a07ccf7d4b31cd00e
GeOnE <emedarias@gmail.com>
2019-08-22 19:46:46 -0300

    CLase 24 mejorada

--- Curso 8/clase24/js/accion.js
+++ Curso 8/clase24/js/accion.js
@@ -6,93 +6,33 @@ function principal()
 		var num2 = 0;
 		var op="";
 		var inp = document.getElementsByTagName("input");
-		inp.mas.onclick=suma;
-		inp.menos.onclick=resta;
-		inp.multi.onclick=multi;
-		inp.divi.onclick=divi;
+		inp.mas.onclick=function(){operacion("+");};
+		inp.menos.onclick=function(){operacion("-");};
+		inp.multi.onclick=function(){operacion("*");};
+		inp.divi.onclick=function(){operacion("/");};
 		inp.igual.onclick=igual;
 		inp.c.onclick=borrar;
-		inp.cero.onclick=cero;
-		inp.uno.onclick=uno;
-		inp.dos.onclick=dos;
-		inp.tres.onclick=tres;
-		inp.cuatro.onclick=cuatro;
-		inp.cinco.onclick=cinco;
-		inp.seis.onclick=seis;
-		inp.siete.onclick=siete;
-		inp.ocho.onclick=ocho;
-		inp.nueve.onclick=nueve;
+		inp.cero.onclick=function(){imprimir(0);};/*Todos los numeros enviar un valor por paramtero a la misma funcion.*/
+		inp.uno.onclick=function(){imprimir(1);};
+		inp.dos.onclick=function(){imprimir(2);};
+		inp.tres.onclick=function(){imprimir(3);};
+		inp.cuatro.onclick=function(){imprimir(4);};
+		inp.cinco.onclick=function(){imprimir(5);};
+		inp.seis.onclick=function(){imprimir(6);};
+		inp.siete.onclick=function(){imprimir(7);};
+		inp.ocho.onclick=function(){imprimir(8);};
+		inp.nueve.onclick=function(){imprimir(9);};
 
-		/*------------------ FUNCIONES QUE ENVIAN SU VALOR A LA PANTALLA ------------------*/
-		function cero()
+		/*------------------ FUNCION QUE ENVIA SU VALOR A LA PANTALLA ------------------*/
+		
+		/*Solo utilizamos una funcion para imprimir los numeros, ya que, el proceso que realiza es siempre el mismo, solo cambia el numero que enviamos por parametro. */
+
+		function imprimir(numero)
 			{
-				var valor=inp.cero.value;
+				var valor=numero;
 				inp.resultado.value+=valor;
-
 			}
-
-		function uno()
-			{
-				var valor=inp.uno.value;
-				inp.resultado.value+=valor;
-
-			}
-
-		function dos()
-			{
-				var valor=inp.dos.value;
-				inp.resultado.value+=valor;
-
-			}
-
-		function tres()
-			{
-				var valor=inp.tres.value;
-				inp.resultado.value+=valor;
-
-			}
-
-		function cuatro()
-			{
-				var valor=inp.cuatro.value;
-				inp.resultado.value+=valor;
-
-			}
-
-		function cinco()
-			{
-				var valor=inp.cinco.value;
-				inp.resultado.value+=valor;
-
-			}
-
-		function seis()
-			{
-				var valor=inp.seis.value;
-				inp.resultado.value+=valor;
-
-			}
-
-		function siete()
-			{
-				var valor=inp.siete.value;
-				inp.resultado.value+=valor;
-
-			}
-
-		function ocho()
-			{
-				var valor=inp.ocho.value;
-				inp.resultado.value+=valor;
-
-			}
-
-		function nueve()
-			{
-				var valor=inp.nueve.value;
-				inp.resultado.value+=valor;
-
-			}
+		
 
 		/*------------------ FUNCIONE QUE BORRA TODOS LOS VALORES IMPRESOS Y GUARDADOS. ------------------*/
 
@@ -106,42 +46,15 @@ function principal()
 			}
 
 		/*------------------ FUNCIONES QUE GUARDAN EL PRIMER VALOR Y DEFINEN LA OPERACION ------------------*/
-		function suma()
+		
+		function operacion(simbolo)
 			{
 				num1=parseInt(inp.resultado.value); /*parseInt() convierte en un numero entero al valor que se le envia.*/
-				op="+";
+				op=simbolo;
 				inp.resultado.value="";
 				inp.cuenta.value+=num1;
 				inp.cuenta.value+=op;
 			}
-
-		function resta()
-			{
-				num1=parseInt(inp.resultado.value);
-				op="-";
-				inp.resultado.value="";
-				inp.cuenta.value+=num1;
-				inp.cuenta.value+=op;
-			}
-
-		function multi()
-			{
-				num1=parseInt(inp.resultado.value);
-				op="*";
-				inp.resultado.value="";
-				inp.cuenta.value+=num1;
-				inp.cuenta.value+=op;
-			}
-
-		function divi()
-			{
-				num1=parseInt(inp.resultado.value);
-				op="/";
-				inp.resultado.value="";
-				inp.cuenta.value+=num1;
-				inp.cuenta.value+=op;
-			}
-
 		
 		/*------------------ FUNCIONES QUE GUARDAN EL SEGUNDO VALOR Y DEFINEN EL RESULTADO ------------------*/
 		
