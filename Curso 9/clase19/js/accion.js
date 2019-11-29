2380ebd8249d6b420679911984193ef978bb5268
GeOnE <emedarias@gmail.com>
2019-11-29 20:44:25 -0300

    Clase 19

--- Curso 9/clase19/js/accion.js
+++ Curso 9/clase19/js/accion.js
@@ -3,6 +3,11 @@ var h3 = document.getElementsByTagName("h3")[0];
 var dias = ["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"];
 var meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
 setInterval(hora,1000);
+var pmam=false;
+
+var boton=document.getElementsByTagName("button");
+boton[0].onclick=function(){pmam=true;};
+boton[1].onclick=function(){pmam=false;};
 
 function hora()
 	{
@@ -13,6 +18,18 @@ function hora()
 		var dia=dias[fecha.getDay()];
 		var mes=meses[fecha.getMonth()];
 		var num=fecha.getDate();
+		var tiempo="AM";
+		if(hora>=12)
+			{	
+				tiempo="PM";
+			}
+		if(pmam==true)
+			{
+				if(hora>12)
+					{
+						hora=hora-12;
+					}
+			}
 		if(hora<10)
 			{
 				hora="0"+hora;
@@ -26,7 +43,21 @@ function hora()
 				sec="0"+sec;
 			}
 		h2.innerHTML=hora+":"+min+":"+sec;
+		if(pmam==true)
+			{
+				h2.innerHTML+=" "+tiempo;
+				h2.className="PmAm";
+			}
+		else
+			{
+				h2.className="";	
+			}
 		h3.innerHTML=dia+" "+num+" de "+mes;
 	}
+
+
+
+
+
 
 
